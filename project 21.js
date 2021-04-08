ffa99ef96e35135bbc05073cda3c3e92e5969c2f
Ronald329 <68265354+ronhlvu@users.noreply.github.com>
2021-04-08 15:55:03 +0530

    Add files via upload

---
+++ project 21.js
@@ -0,0 +1,30 @@
+function is_touching(){
+    //write code to bounce off ball from the block1 
+    if(block1.isTouching(ball) && ball.bounceOff(block1)){
+        ball.shapeColor = "blue";
+        music.play();
+    }
+  //write code to bounce off ball from the block3
+    if(block3.isTouching(ball) && ball.bounceOff(block3)){
+        ball.shapeColor = "green";
+        music.play();
+    }
+   //write code to bounce off ball from the block4
+    if(block4.isTouching(ball) && ball.bounceOff(block4)){
+        ball.shapeColor = "red";
+        music.play();
+    }
+
+
+    if(block2.isTouching(ball)){
+        ball.shapeColor = "orange";
+        //write code to set velocityX and velocityY of ball as 0
+        ball.velocityX=0
+        ball.velocityY=0
+        }
+
+    
+
+
+    
+}
---
+++ sketch.js
@@ -0,0 +1,43 @@
+var canvas;
+var block1,block2,block3,block4;
+var ball, edges;
+var music;
+
+function preload(){
+    // load sound here
+}
+
+
+function setup(){
+    canvas = createCanvas(850,600);
+
+    block1 = createSprite(0,580,360,30);
+    block1.shapeColor = "blue";
+
+    block2 = createSprite(295,580,200,30);
+    block2.shapeColor = "orange";
+
+    block3 = createSprite(518,580,200,30);
+    block3.shapeColor = "red";
+
+    block4 = createSprite(750,580,200,30);
+    block4.shapeColor = "green";
+
+    //create two more blocks i.e. block3 and block4 here
+
+    ball = createSprite(random(20,750),100, 40,40);
+    ball.shapeColor = rgb(255,255,255);
+    ball.velocityX=6
+    ball.velocityY=-6
+    //write code to add velocityX and velocityY
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    edges=createEdgeSprites();
+    ball.bounceOff(edges);
+    is_touching()
+    
+    drawSprites();
+}
